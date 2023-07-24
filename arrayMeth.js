b780abc7738c156d6bbc53a93e1a5760d18fabc5
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-07-24 12:05:02 +0530

    toString, join

---
+++ arrayMeth.js
@@ -0,0 +1,15 @@
+// 1. toString()
+let cars = ['audi', 'benz', 'bmw', 'toyota']
+let carNames = cars.toString();
+//console.log(carNames)
+//ans- audi,benz,bmw,toyota
+//The toString() method is a JavaScript library function used to convert the given object to its corresponding string representation. toString in javascript can be used with numbers, strings, arrays, and objects.
+
+
+// 2. join()
+let carsNames = ['audi', 'benz', 'bmw', 'toyota']
+let joinCars = carsNames.join(' and ')
+// console.log(joinCars)
+//The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string.
+
+
