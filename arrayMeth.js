9fb6bfc99227e5408736fb250724a51444217eec
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-07-24 16:16:16 +0530

    concat, splice

--- arrayMeth.js
+++ arrayMeth.js
@@ -10,6 +10,28 @@ let carNames = cars.toString();
 let carsNames = ['audi', 'benz', 'bmw', 'toyota']
 let joinCars = carsNames.join(' and ')
 // console.log(joinCars)
+//audi and benz and bmw and toyota
 //The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string.
 
 
+// 3. concat()
+let names = ['vinay', 'paru', 'rudra']
+let names2 = ['adarsh', 'preethi', 'aami']
+let concatNames = names.concat(names2)
+// console.log(concatNames)
+// ans- [ 'vinay', 'paru', 'rudra', 'adarsh', 'preethi', 'aami' ]
+// The concat() method is used to merge two or more arrays. 
+//This method does not change the existing arrays, but instead returns a new array.
+
+// 4. splice()
+let list = ['arun', 'subu', 'kannan', 'kichu']
+// let splicedList = list.splice(1, 2);
+// console.log(splicedList)
+//ans-  [ 'subu', 'kannan' ]
+// console.log(list)
+// ans- [ 'arun', 'kichu' ]
+// let addedSplicedList = list.splice(1, 2, 'vinay','adarsh')
+// console.log(addedSplicedList)
+// ans- [ 'subu', 'kannan' ]
+// console.log(list)
+// ans-[ 'arun', 'vinay', 'adarsh', 'kichu' ]
